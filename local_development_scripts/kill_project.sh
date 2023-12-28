deafa0fa76256d31cf5fb2e594fee0c3ec700fed
marco-mart <martinez.marco.1027@gmail.com>
2023-12-27 17:09:49 -0700

    Added to kill script. Stops database servers now.

--- local_development_scripts/kill_project.sh
+++ local_development_scripts/kill_project.sh
@@ -5,10 +5,18 @@ auth_pid=$(cat auth_pid.txt)
 gamestore_pid=$(cat gamestore_pid.txt)
 
 # Kill the services using their PIDs.
-echo "Stopping auth_service (PID: $auth_pid)..."
+echo "Stopping auth service (PID: $auth_pid)..."
 kill $auth_pid
 
-echo "Stopping gamestore_service (PID: $gamestore_pid)..."
+echo "Stopping gamestore service (PID: $gamestore_pid)..."
 kill $gamestore_pid
 
 echo "Services stopped."
+
+# Stop the database servers.
+
+echo "Stopping gamestore database"
+docker stop mysql_local_game_store
+
+echo "Stopping auth service database"
+docker stop mysql_local_auth_service
