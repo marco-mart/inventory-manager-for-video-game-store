b30e0c986ba742f61777a3f8f00cc6dd413c2336
marco-mart <martinez.marco.1027@gmail.com>
2024-01-03 15:52:24 -0700

    Add initial Angular web interface project, update codebase notes.

---
+++ web-interface/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `
+
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'web-interface';
+}
