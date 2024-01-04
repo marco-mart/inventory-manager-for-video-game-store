9e3609d3f40e2eadf78fdcc48589c29969ebb40e
marco-mart <martinez.marco.1027@gmail.com>
2024-01-03 17:41:14 -0700

    Add web interface, Tailwind CSS, and update codebase notes.

--- web-interface/src/app/app.component.ts
+++ web-interface/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   template: `
-
+    <h1 class="text-3xl font-bold underline">
+      Hello World!
+    </h1>
   `,
-  styles: [],
+  styles: []
 })
 export class AppComponent {
   title = 'web-interface';
